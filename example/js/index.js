1b126430130c192ccde329a1f68690fd4fe5a29d
SilunWang <wangsl11@mails.tsinghua.edu.cn>
2014-06-09 02:48:05 +0800

    add two buttons

--- example/js/index.js
+++ example/js/index.js
@@ -1,8 +1,16 @@
 // index.js
 var coffeeparser = parser;
-
+var QScode = $("#coffee-text").text();
+var BScode = $("#coffee-BS-text").text();
 function exec (input) {
 	IndentLexer.init(input);
 	var s = IndentLexer.scan();
     return format(addDeclare(coffeeparser.parse(s)));
+};
+changetoBS = function() {
+	$("#coffee-text").text(BScode);
+};
+
+changetoQS = function() {
+	$("#coffee-text").text(QScode);
 }
